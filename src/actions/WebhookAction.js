7ef83b3835a202c8b3b95a66ea3e4ee4a2d47e5c
Zack Urben <zackurben@gmail.com>
2016-08-12 17:04:39 -0500

    Adding debugging to webhook action.

--- src/actions/WebhookAction.js
+++ src/actions/WebhookAction.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var rest = require('restler');
+var _ = require('lodash');
+var debug = require('debug')('formio:action:webhook');
 
 module.exports = function(router) {
   var Action = router.formio.Action;
@@ -72,40 +74,72 @@ module.exports = function(router) {
   /**
    * Trigger the webhooks.
    *
+   * @param handler
+   * @param method
    * @param req
    *   The Express request object.
    * @param res
    *   The Express response object.
-   * @param cb
+   * @param next
    *   The callback function to execute upon completion.
    */
   WebhookAction.prototype.resolve = function(handler, method, req, res, next) {
     var options = {};
-    if (this.settings.username) {
-      options.username = this.settings.username;
-      options.password = this.settings.password;
+    debug(_.get(this, 'settings'));
+
+    // Get the settings
+    if (_.has(this, 'settings.username')) {
+      options.username = _.get(this, 'settings.username');
     }
+    if (_.has(this, 'settings.password')) {
+      options.password = _.get(this, 'settings.password');
+    }
+
+    // Cant send a webhook if the url isnt set.
+    if (!_.has(this, 'settings.url')) {
+      debug('No url given in the settings');
+      return next();
+    }
+
     if (req.method.toLowerCase() === 'delete') {
       options.query = req.params;
-      rest.del(this.settings.url, options);
+      return rest.del(this.settings.url, options)
+        .on('response', function(event) {
+          debug(event);
+        })
+        .on('error', function(event) {
+          debug(event);
+        });
     }
-    else {
-      // Setup the post data.
-      var postData = {
-        request: req.body,
-        response: res.resource,
-        params: req.params
-      };
 
-      // Make the request.
-      switch (req.method.toLowerCase()) {
-        case 'post':
-          rest.postJson(this.settings.url, postData, options);
-          break;
-        case 'put':
-          rest.putJson(this.settings.url, postData, options);
-          break;
-      }
+    // Setup the post data.
+    var postData = {
+      request: req.body,
+      response: res.resource,
+      params: req.params
+    };
+
+    // Make the request.
+    debug('Request: ' + req.method.toLowerCase());
+    switch (req.method.toLowerCase()) {
+      case 'post':
+        rest.postJson(this.settings.url, postData, options)
+          .on('response', function(event) {
+            debug(event);
+          })
+          .on('error', function(event) {
+            debug(event);
+          });
+        break;
+      case 'put':
+        rest.putJson(this.settings.url, postData, options)
+          .on('response', function(event) {
+            debug(event);
+          })
+          .on('error', function(event) {
+            debug(event);
+          });
+        break;
     }
 
     next();
