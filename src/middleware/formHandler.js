2413d1e3ecce3b0e50ff810ce3b9a96f2dc31a93
Zack Urben <zackurben@gmail.com>
2016-09-22 10:47:56 -0500

    Fixing the formHandler to handle form paths correctly.

--- src/middleware/formHandler.js
+++ src/middleware/formHandler.js
@@ -8,24 +8,29 @@
  */
 module.exports = function(router) {
   var hook = require('../util/hook')(router.formio);
+  var formio = hook.alter('formio', router.formio);
+
   return function formHandler(req, res, next) {
     if (req.method === 'POST' && req.body.path) {
-      var pathParts = req.body.path.split('/');
-      if (router.formio.config.reservedForms.indexOf(pathParts[0]) !== -1) {
+      var fragments = req.body.path.split('/');
+      if (
+        formio.config.reservedForms.indexOf(req.body.path) !== -1 // Check the full path
+        || formio.config.reservedForms.indexOf(fragments[0]) !== -1 // check the first path fragment if available
+      ) {
         var message = 'Form path cannot contain one of the following names: ';
-        return res.status(400).send(message + router.formio.config.reservedForms.join(', '));
+        return res.status(400).send(message + formio.config.reservedForms.join(', '));
       }
     }
 
     if ((req.method === 'POST' || req.method === 'PUT') && req.body.components) {
       var badCharacters = /^[^A-Za-z]+|[^A-Za-z0-9\-\.]+/g;
       var error = false;
-      router.formio.util.eachComponent(req.body.components, function(component) {
+      formio.util.eachComponent(req.body.components, function(component) {
         // Remove all unsupported characters from api keys.
         if (component.hasOwnProperty('key')) {
           component.key = component.key.replace(badCharacters, '');
         }
-        if (component.key === '' && !router.formio.util.isLayoutComponent(component)) {
+        if (component.key === '' && !formio.util.isLayoutComponent(component)) {
           error = true;
         }
       }, true);
