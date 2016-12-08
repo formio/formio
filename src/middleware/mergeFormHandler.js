94dd0a8879b3c47d5b11b749bee623b2c0ab292c
Zack Urben <zackurben@gmail.com>
2016-12-08 11:43:45 -0600

    Updating form merge to work with tables, adding table merge tests.

--- src/middleware/mergeFormHandler.js
+++ src/middleware/mergeFormHandler.js
@@ -46,7 +46,7 @@ module.exports = function(router) {
 
       // Build the new component list using the oldest components first.
       var final = _.cloneDeep(_components.stable);
-      var ignoreTypes = ['panel', 'columns', 'fieldset'];
+      var ignoreTypes = ['panel', 'columns', 'fieldset', 'table'];
       var ignoredComponents = [];
       ['stable', 'local'].forEach(function(type) {
         util.eachComponent(_components[type], function(component, path) {
