f8326acd080ffb8364a8487b60ac2eb3d95807e5
Zack Urben <zackurben@gmail.com>
2017-03-31 15:14:11 -0500

    Adding tests for importing templates without any forms defined.

--- test/templates.js
+++ test/templates.js
@@ -1867,12 +1867,12 @@ module.exports = (app, template, hook) => {
         });
 
         it('The template should not export any resources', function(done) {
-          assert(exportData.resources, {});
+          assert.deepEqual(exportData.resources, {});
           checkTemplateFormsAndResources(project, 'resource', exportData.resources, done);
         });
 
         it('The template should not export any actions', function(done) {
-          assert(exportData.actions, {});
+          assert.deepEqual(exportData.actions, {});
           checkTemplateActions(project, exportData.actions, done);
         });
 
@@ -1982,17 +1982,17 @@ module.exports = (app, template, hook) => {
         });
 
         it('The template should not export any forms', function(done) {
-          assert(exportData.forms, {});
+          assert.deepEqual(exportData.forms, {});
           checkTemplateFormsAndResources(project, 'form', exportData.forms, done);
         });
 
         it('The template should not export any resources', function(done) {
-          assert(exportData.resources, {});
+          assert.deepEqual(exportData.resources, {});
           checkTemplateFormsAndResources(project, 'resource', exportData.resources, done);
         });
 
         it('The template should not export any actions', function(done) {
-          assert(exportData.actions, {});
+          assert.deepEqual(exportData.actions, {});
           checkTemplateActions(project, exportData.actions, done);
         });
 
@@ -2098,12 +2098,12 @@ module.exports = (app, template, hook) => {
         });
 
         it('The template should not export any roles', function(done) {
-          assert(exportData.resources, {});
+          assert.deepEqual(exportData.resources, {});
           checkTemplateRoles(project, exportData.roles, done);
         });
 
         it('The template should not export any forms', function(done) {
-          assert(exportData.forms, {});
+          assert.deepEqual(exportData.forms, {});
           checkTemplateFormsAndResources(project, 'form', exportData.forms, done);
         });
 
@@ -2113,7 +2113,7 @@ module.exports = (app, template, hook) => {
         });
 
         it('The template should not export any actions', function(done) {
-          assert(exportData.actions, {});
+          assert.deepEqual(exportData.actions, {});
           checkTemplateActions(project, exportData.actions, done);
         });
 
@@ -2121,6 +2121,127 @@ module.exports = (app, template, hook) => {
           assert.deepEqual(exportData.resources, {});
           assert.equal(exportData.version, '2.0.0');
           assert.deepEqual(_.omit(exportData, ['plan', 'version', 'resources']), _.omit(testTemplate, ['plan', 'version', 'resources']));
+        });
+      });
+
+      before(function(done) {
+        template.clearData(done);
+      });
+
+      after(function(done) {
+        template.clearData(done);
+      });
+    });
+
+    describe('missingFormResources Template', function() {
+      let testTemplate = require('./fixtures/templates/missingFormResources.json');
+      let _template = _.cloneDeep(testTemplate);
+
+      describe('Import', function() {
+        let project = {};
+
+        it('Should be able to bootstrap the default template', function(done) {
+          app.formio.template.import(_template, (err) => {
+            if (err) {
+              return done(err);
+            }
+
+            done();
+          });
+        });
+
+        it('All the roles should be imported', function(done) {
+          checkTemplateRoles(project, testTemplate.roles, done);
+        });
+
+        it('No forms should be imported', function(done) {
+          assert.deepEqual(testTemplate.forms, undefined);
+          checkTemplateFormsAndResources(project, 'form', testTemplate.forms, done);
+        });
+
+        it('All the resources should be imported', function(done) {
+          checkTemplateFormsAndResources(project, 'resource', testTemplate.resources, done);
+        });
+
+        it('All the actions should be imported', function(done) {
+          checkTemplateActions(project, testTemplate.actions, done);
+        });
+      });
+
+      describe('Export', function() {
+        let project = {};
+        let exportData = {};
+
+        it('Should be able to export project data', function(done) {
+          app.formio.template.export(testTemplate, (err, data) => {
+            if (err) {
+              return done(err);
+            }
+
+            exportData = data;
+            return done();
+          });
+        });
+
+        it('An export should contain the export title', function() {
+          assert.equal(
+            hook.alter('exportTitle', 'Export', exportData),
+            'Export'
+          );
+        });
+
+        it('An export should contain the current export version', function() {
+          assert.equal(
+            exportData.version,
+            '2.0.0'
+          );
+        });
+
+        it('An export should contain the description', function() {
+          assert.equal(
+            hook.alter('exportDescription', '', exportData),
+            ''
+          );
+        });
+
+        it('An export should contain the export name', function() {
+          assert.equal(
+            hook.alter('exportName', 'export', exportData),
+            'export'
+          );
+        });
+
+        it('An export should contain the export plan', function() {
+          assert.equal(
+            hook.alter('exportPlan', 'community', exportData),
+            'community'
+          );
+        });
+
+        it('The template should not export any roles', function(done) {
+          assert.deepEqual(exportData.resources, {});
+          checkTemplateRoles(project, exportData.roles, done);
+        });
+
+        it('The template should not export any forms', function(done) {
+          assert.deepEqual(exportData.forms, {});
+          checkTemplateFormsAndResources(project, 'form', exportData.forms, done);
+        });
+
+        it('The template should not export any resources', function(done) {
+          assert.deepEqual(exportData.resources, {});
+          checkTemplateFormsAndResources(project, 'resource', exportData.resources, done);
+        });
+
+        it('The template should not export any actions', function(done) {
+          assert.deepEqual(exportData.actions, {});
+          checkTemplateActions(project, exportData.actions, done);
+        });
+
+        it('An export should match an import', function() {
+          assert.deepEqual(exportData.forms, {});
+          assert.equal(exportData.version, '2.0.0');
+          assert.deepEqual(_.omit(exportData, ['plan', 'version', 'forms']), _.omit(testTemplate, ['plan', 'version', 'forms']));
         });
       });
 
