7258902dc76e16945d9e1e71aeea12837c890833
douglaselee <douglaselee@comcast.net>
2017-07-12 11:51:55 -0400

    Pass components with action condition textarea for ACE editor autocomplete

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -424,6 +424,7 @@ module.exports = function(router) {
                         type: 'textarea',
                         input: true,
                         key: 'custom',
+                        components: form.components,
                         placeholder: customPlaceHolder
                       }
                     ]
