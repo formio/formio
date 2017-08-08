47a3761b26203714c3abafdaec48fafd3f0f0415
douglaselee <douglaselee@comcast.net>
2017-08-08 11:47:13 -0400

    Moved logic to new field action for form components

--- src/actions/fields/index.js
+++ src/actions/fields/index.js
@@ -4,6 +4,7 @@ module.exports = function(router) {
   return {
     signature: require('./signature')(router.formio),
     password: require('./password')(router.formio),
+    form: require('./form')(router.formio),
     email: require('./email')(router.formio)
   };
 };
