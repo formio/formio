5ec5d5c01d21d8ad413b0c2235663bc80b08d800
Travis Tidwell <travis@form.io>
2017-11-16 14:14:02 -0600

    FOR-804: Added backend validation check for select dropdown components.

--- test/helper.js
+++ test/helper.js
@@ -12,6 +12,7 @@ module.exports = function(app) {
   var Helper = function(owner, template, hook) {
     this.contextName = '';
     this.lastSubmission = null;
+    this.lastResponse = null;
     this.owner = owner;
     this.series = [];
     this.template = template || {
@@ -52,6 +53,7 @@ module.exports = function(app) {
         _.each(res.body, function(form) {
           this.template.forms[form.name] = form;
         }.bind(this));
+        this.lastResponse = res;
         this.owner.token = res.headers['x-jwt-token'];
         done(null, this.template.forms);
       }.bind(this));
@@ -64,7 +66,7 @@ module.exports = function(app) {
 
     return null;
   };
-  
+
   Helper.prototype.getAction = function(form, action) {
     if (!action && form) {
       if (typeof form ==='string') {
@@ -93,7 +95,7 @@ module.exports = function(app) {
         return;
       }
     });
-    
+
     return _action;
   };
 
@@ -130,6 +132,7 @@ module.exports = function(app) {
         }.bind(this));
         assert.equal(Object.keys(this.template.roles).length, 3);
         this.owner.token = res.headers['x-jwt-token'];
+        this.lastResponse = res;
         done(null, this.template.roles);
       }.bind(this));
   };
@@ -174,7 +177,7 @@ module.exports = function(app) {
         if (err) {
           return done(err);
         }
-
+        this.lastResponse = res;
         this.template.project = res.body;
         return done(null, res.body);
       }.bind(this));
@@ -198,6 +201,7 @@ module.exports = function(app) {
             return done(err);
           }
 
+          this.lastResponse = res;
           this.template.project = res.body;
           this.getRolesAndForms(function(err) {
             if (err) {
@@ -282,6 +286,7 @@ module.exports = function(app) {
         if (err) {
           return done(err, res);
         }
+        this.lastResponse = res;
         this.owner.token = res.headers['x-jwt-token'];
         this.template.forms[form.name] = res.body;
         done(null, res.body);
@@ -413,6 +418,7 @@ module.exports = function(app) {
           return done('No response', res)
         }
 
+        this.lastResponse = res;
         this.owner.token = res.headers['x-jwt-token'];
         for (var a = 0; a < this.template.actions[form].length; a++) {
           if (
@@ -476,6 +482,7 @@ module.exports = function(app) {
           return done(err);
         }
 
+        this.lastResponse = res;
         this.owner.token = res.headers['x-jwt-token'];
         if (!this.template.actions[form]) {
           this.template.actions[form] = [];
@@ -508,6 +515,7 @@ module.exports = function(app) {
 
         this.owner.token = res.headers['x-jwt-token'];
         this.lastSubmission = res.body;
+        this.lastResponse = res;
         _.each(this.template.submissions, function(sub, form) {
           if (sub._id === submission._id) {
             this.template.submissions[form] = res.body;
@@ -552,6 +560,7 @@ module.exports = function(app) {
         }
 
         this.lastSubmission = res.body;
+        this.lastResponse = res;
         this.template.submissions[form].push(res.body);
         done(null, res.body);
       }.bind(this));
@@ -594,6 +603,7 @@ module.exports = function(app) {
           }
 
           var response = res.body;
+          this.lastResponse = res;
           this.owner.token = res.headers['x-jwt-token'];
           this.template.roles = this.template.roles || {};
           this.template.roles[response.title] = response;
@@ -613,6 +623,7 @@ module.exports = function(app) {
           }
 
           var response = res.body;
+          this.lastResponse = res;
           this.owner.token = res.headers['x-jwt-token'];
           this.template.roles = this.template.roles || {};
           this.template.roles[response.title] = response;
@@ -649,6 +660,7 @@ module.exports = function(app) {
         }
 
         var response = res.body;
+        this.lastResponse = res;
         this.owner.token = res.headers['x-jwt-token'];
         this.template.roles = this.template.roles || {};
         delete this.template.roles[response.title];
