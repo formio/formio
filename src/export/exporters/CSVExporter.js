877fbb46b8c5efb1ab91ecfe7a75e146d665a2d7
Travis Tidwell <travis@form.io>
2017-12-20 10:34:14 -0600

    Fixed an issue with the CSV export for fields within datagrids.

--- src/export/exporters/CSVExporter.js
+++ src/export/exporters/CSVExporter.js
@@ -25,101 +25,122 @@ var CSVExporter = function(form, req, res) {
   this.fields = [];
 
   var ignore = ['password', 'button', 'container', 'datagrid'];
-  util.eachComponent(form.components, function(component, path) {
-    if (!component.input || !component.key || ignore.indexOf(component.type) !== -1) {
-      return;
-    }
+  try {
+    util.eachComponent(form.components, function(component, path) {
+      if (!component.input || !component.key || ignore.indexOf(component.type) !== -1) {
+        return;
+      }
 
-    var items = [];
+      var items = [];
 
-    // If a component has multiple parts, pick what we want.
-    if (component.type === 'address') {
-      items.push({
-        path: 'formatted_address',
-        rename: 'formatted'
-      });
-      items.push({
-        path: 'geometry.location.lat',
-        rename: 'lat'
-      });
-      items.push({
-        path: 'geometry.location.lng',
-        rename: 'lng'
-      });
-    }
-    else if (component.type === 'selectboxes') {
-      _.each(component.values, function(option) {
-        items.push({label: [path, option.value].join('.'), path: option.value, type: 'boolean'});
-      });
-    }
-    else if (component.type === 'checkbox') {
-      items.push({type: 'boolean'});
-    }
-    else if (component.type === 'survey') {
-      _.each(component.questions, function(question) {
-        items.push({label: [path, question.value].join('.'), path: question.value});
-      });
-    }
-    else if (['select', 'resource'].indexOf(component.type) !== -1) {
-      // Prepare the Lodash template by deleting tags and html entities
-      var clearTemplate = Entities.decode(component.template.replace(/<\/?[^>]+(>|$)/g, ''));
-      var templateExtractor = _.template(clearTemplate, {
-        variable: 'item'
-      });
+      // If a component has multiple parts, pick what we want.
+      if (component.type === 'address') {
+        items.push({
+          path: 'formatted_address',
+          rename: 'formatted'
+        });
+        items.push({
+          path: 'geometry.location.lat',
+          rename: 'lat'
+        });
+        items.push({
+          path: 'geometry.location.lng',
+          rename: 'lng'
+        });
+      }
+      else if (component.type === 'selectboxes') {
+        _.each(component.values, function(option) {
+          items.push({label: [path, option.value].join('.'), path: option.value, type: 'boolean'});
+        });
+      }
+      else if (component.type === 'checkbox') {
+        items.push({type: 'boolean'});
+      }
+      else if (component.type === 'survey') {
+        _.each(component.questions, function(question) {
+          items.push({label: [path, question.value].join('.'), path: question.value});
+        });
+      }
+      else if (['select', 'resource'].indexOf(component.type) !== -1) {
+        // Prepare the Lodash template by deleting tags and html entities
+        var clearTemplate = Entities.decode(component.template.replace(/<\/?[^>]+(>|$)/g, ''));
+        var templateExtractor = _.template(clearTemplate, {
+          variable: 'item'
+        });
 
-      var valuesExtractor = function(value) {
-          //checking if the component can accept multiple values.
-          if (component.multiple) {
-              if (component.type === 'resource') {
-                  var tempVal = [];
-                  _.each(value, function(eachItem) {
-                      tempVal.push(templateExtractor(eachItem));
-                  });
-                  return tempVal;
+        var valuesExtractor = function(value) {
+          // Check if this is within a datagrid.
+          if (_.isArray(value) && value[0] && value[0][component.key]) {
+            let rowValues = [];
+            _.each(value, (row) => {
+              if (!row) {
+                return;
               }
-              else if (component.type === 'select') {
-                  return value;
+              let rowValue = row[component.key];
+              if (rowValue) {
+                rowValues.push(valuesExtractor(rowValue));
               }
+            });
+            return rowValues;
           }
           else {
+            //checking if the component can accept multiple values.
+            if (component.multiple) {
+              if (component.type === 'resource') {
+                var tempVal = [];
+                _.each(value, function(eachItem) {
+                  tempVal.push(templateExtractor(eachItem));
+                });
+                return tempVal;
+              }
+              else if (component.type === 'select') {
+                return value;
+              }
+            }
+            else {
               return templateExtractor(value);
+            }
           }
-      };
-      items.push({
-        preprocessor: function(value) {
-          return _.isObject(value)
-            ? valuesExtractor(value)
-            : value;
+        };
+        items.push({
+          preprocessor: function(value) {
+            return _.isObject(value)
+              ? valuesExtractor(value)
+              : value;
+          }
+        });
+      }
+      else {
+        // Default to the current component item.
+        items.push({});
+      }
+
+      items.forEach(function(item) {
+        var finalItem = {
+          component: path,
+          path: item.path || component.key,
+          label: item.label || path
+        };
+
+        if (item.hasOwnProperty('rename')) {
+          finalItem.rename = item.rename;
         }
-      });
-    }
-    else {
-      // Default to the current component item.
-      items.push({});
-    }
 
-    items.forEach(function(item) {
-      var finalItem = {
-        component: path,
-        path: item.path || component.key,
-        label: item.label || path
-      };
+        if (item.hasOwnProperty('type')) {
+          finalItem.type = item.type;
+        }
 
-      if (item.hasOwnProperty('rename')) {
-        finalItem.rename = item.rename;
-      }
+        if (item.hasOwnProperty('preprocessor')) {
+          finalItem.preprocessor = item.preprocessor;
+        }
 
-      if (item.hasOwnProperty('type')) {
-        finalItem.type = item.type;
-      }
-
-      if (item.hasOwnProperty('preprocessor')) {
-        finalItem.preprocessor = item.preprocessor;
-      }
-
-      this.fields.push(finalItem);
+        this.fields.push(finalItem);
+      }.bind(this));
     }.bind(this));
-  }.bind(this));
+  }
+  catch (err) {
+    res.status(500).send(err.message || err);
+  }
 };
 
 CSVExporter.prototype = Object.create(Exporter.prototype);
