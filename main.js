fac1a516d77e51f6bf0d8e8cb5356a3440fec3f5
Yury Rybak <Yury.Rybak@Softensity.com>
2018-01-16 13:36:11 +0300

    Refactoring: fixed redundant spaces in template strings.

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 var util = require('./src/util/util');
 require('colors');
 require('./server')().then(function(state) {
-  util.log(` > Serving the Form.io API Platform at ${  state.config.domain.green}`);
+  util.log(` > Serving the Form.io API Platform at ${state.config.domain.green}`);
   state.server.listen(state.config.port);
 });
