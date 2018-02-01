aba4336e2e0ec3d7a6e0b69232bfeee910f75986
Randall Knutson <randall@form.io>
2018-01-31 22:51:02 -0600

    Fix the way rows are found for datagrid checkConditionals.

--- src/resources/Validator.js
+++ src/resources/Validator.js
@@ -770,25 +770,25 @@ class Validator {
             // Walk up the path tree to determine if the component is hidden.
             const result = detail.path.reduce((result, key) => {
               result.path.push(key);
-              if (isNaN(key)) {
-                const component = components[result.path.join('.')];
 
-                // Form "data" keys don't have components.
-                if (component) {
-                  result.hidden = result.hidden ||
-                    !checkConditional(component, _.get(value, result.path.slice(0, result.path.length - 1)), result.submission, true);
+              const component = components[result.path.filter(isNaN).join('.')];
 
-                  const clearOnHide = util.isBoolean(component.clearOnHide) ?
-                    util.boolean(component.clearOnHide) : true;
+              // Form "data" keys don't have components.
+              if (component) {
+                result.hidden = result.hidden ||
+                  !checkConditional(component,
+                    _.get(value, result.path.slice(0, result.path.length - 1)), result.submission, true);
 
-                  if (clearOnHide && result.hidden) {
-                    _.unset(value, result.path);
-                  }
+                const clearOnHide = util.isBoolean(component.clearOnHide) ?
+                  util.boolean(component.clearOnHide) : true;
+
+                if (clearOnHide && result.hidden) {
+                  _.unset(value, result.path);
                 }
-                else {
-                  // Since this is a subform, change the submission object going to the conditionals.
-                  result.submission = _.get(value, result.path);
-                }
+              }
+              else {
+                // Since this is a subform, change the submission object going to the conditionals.
+                result.submission = _.get(value, result.path);
               }
 
               return result;
