89fead4b2a8bde2ad103e495af2822a76cb526e0
Travis Tidwell <travis@form.io>
2019-01-03 11:40:29 -0600

    Fixing the resource access searches.

--- src/middleware/submissionResourceAccessFilter.js
+++ src/middleware/submissionResourceAccessFilter.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 const debug = require('debug')('formio:middleware:submissionResourceAccessFilter');
+const EVERYONE = '000000000000000000000000';
 
 module.exports = function(router) {
   return function submissionResourceAccessFilter(req, res, next) {
@@ -37,15 +38,26 @@ module.exports = function(router) {
 
     const userId = _.get(req, 'user._id');
     const search = userRoles.map(util.idToBson.bind(util));
+    search.push(util.idToBson(EVERYONE));
     if (userId) {
       search.push(util.idToBson(userId));
     }
-    hook.alter('resourceAccessFilter', search, req, function(err, search) {
+    hook.alter('resourceAccessFilter', search, req, function(err, newSearch) {
       // Try to recover if the hook fails.
       if (err) {
         debug(err);
       }
 
+      // If not search is provided, then just default to the user or everyone if there is no user.
+      if (!newSearch || !newSearch.length) {
+        newSearch = [];
+        newSearch.push(util.idToBson(EVERYONE));
+        if (userId) {
+          newSearch.push(util.idToBson(userId));
+        }
+      }
+
+      // Perform our search.
       let query = null;
       if (userId) {
         query = {
@@ -54,7 +66,7 @@ module.exports = function(router) {
           $or: [
             {
               'access.type': {$in: ['read', 'write', 'admin']},
-              'access.resources': {$in: search}
+              'access.resources': {$in: newSearch}
             },
             {
               owner: util.idToBson(userId)
@@ -67,7 +79,7 @@ module.exports = function(router) {
           form: util.idToBson(req.formId),
           deleted: {$eq: null},
           'access.type': {$in: ['read', 'write', 'admin']},
-          'access.resources': {$in: search}
+          'access.resources': {$in: newSearch}
         };
       }
 
