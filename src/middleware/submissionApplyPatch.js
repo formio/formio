9133eca58082b8fb795ce5ef89b5b0eba0a8bb22
Randall Knutson <randall@knutson.io>
2019-03-13 12:29:00 -0500

    Switch the way that patch loads submission to make compatible with encryption.

--- src/middleware/submissionApplyPatch.js
+++ src/middleware/submissionApplyPatch.js
@@ -1,16 +1,30 @@
 'use strict';
 const jsonPatch = require('fast-json-patch');
+const _ = require('lodash');
 
 module.exports = router => (req, res, next) => {
   if (req.method !== 'PATCH') {
     return next();
   }
 
-  router.formio.cache.loadSubmission(req, req.formId, req.subId, function(err, submission) {
+  const childReq = router.formio.util.createSubRequest(req);
+  if (!childReq) {
+    return res.headersSent ? next() : res.status(400).json('Too many recursive requests.');
+  }
+  childReq.method = 'GET';
+
+  const childRes = router.formio.util.createSubResponse((err) => {
+    if (childRes.statusCode > 299) {
+      return res.headersSent ? next() : res.status(childRes.statusCode).json(err);
+    }
+  });
+
+  router.resourcejs[req.route.path]['get'](childReq, childRes, function(err) {
     if (err) {
-      return res.sendStatus(400);
+      return res.status(400).send(err);
     }
 
+    const submission = _.get(childReq, 'resource.item', false);
     // No submission exists.
     if (!submission) {
       return res.sendStatus(404);
