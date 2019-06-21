3ec241a656dcdd259054cb92cdf0e10461412fd3
Daniel Bueno <danielbuenokeys@gmail.com>
2019-06-21 13:35:22 -0300

    access control allow origin header added

--- server.js
+++ server.js
@@ -50,6 +50,19 @@ module.exports = function(options) {
     express: app
   });
 
+
+  //cors configuration
+  app.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(config.allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
   // Mount the client application.
   app.use('/', express.static(path.join(__dirname, '/client/dist')));
 
