5da6b6d01c9ef79b0e0c15ddd635f5e640eba780
Travis Tidwell <travis@form.io>
2020-02-10 10:40:07 -0600

    Fixing issue where undefined components would cause server to crash.

--- src/middleware/submissionHandler.js
+++ src/middleware/submissionHandler.js
@@ -221,68 +221,69 @@ module.exports = (router, resourceName, resourceId) => {
     // This should probably be moved to utils.
     function eachValue(components, data, fn, context, path = '') {
       const promises = [];
-
-      components.forEach((component) => {
-        if (component.hasOwnProperty('components') && Array.isArray(component.components)) {
-          // If tree type is an array of objects like datagrid and editgrid.
-          if (['datagrid', 'editgrid'].includes(component.type) || component.arrayTree) {
-            _.get(data, component.key, []).forEach((row, index) => {
-              promises.push(eachValue(
-                component.components,
-                row,
-                fn,
-                context,
-                `${path ? `${path}.` : ''}${component.key}[${index}]`,
-              ));
-            });
-          }
-          else if (['form'].includes(component.type)) {
-            promises.push(eachValue(
-              component.components,
-              _.get(data, `${component.key}.data`, {}),
-              fn,
-              context,
-              `${path ? `${path}.` : ''}${component.key}.data`,
-            ));
-          }
-          else if (
-            ['container'].includes(component.type) ||
-            (
-              component.tree &&
-              !['panel', 'table', 'well', 'columns', 'fieldset', 'tabs', 'form'].includes(component.type)
-            )
-          ) {
-            promises.push(eachValue(
-              component.components,
-              _.get(data, component.key),
-              fn,
-              context,
-              `${path ? `${path}.` : ''}${component.key}`,
-            ));
-          }
-          else {
-            promises.push(eachValue(component.components, data, fn, context, path));
-          }
-        }
-        else if (component.hasOwnProperty('columns') && Array.isArray(component.columns)) {
-          // Handle column like layout components.
-          component.columns.forEach((column) => {
-            promises.push(eachValue(column.components, data, fn, context, path));
-          });
-        }
-        else if (component.hasOwnProperty('rows') && Array.isArray(component.rows)) {
-          // Handle table like layout components.
-          component.rows.forEach((row) => {
-            if (Array.isArray(row)) {
-              row.forEach((column) => {
-                promises.push(eachValue(column.components, data, fn, context, path));
+      if (components) {
+        components.forEach((component) => {
+          if (component.hasOwnProperty('components') && Array.isArray(component.components)) {
+            // If tree type is an array of objects like datagrid and editgrid.
+            if (['datagrid', 'editgrid'].includes(component.type) || component.arrayTree) {
+              _.get(data, component.key, []).forEach((row, index) => {
+                promises.push(eachValue(
+                  component.components,
+                  row,
+                  fn,
+                  context,
+                  `${path ? `${path}.` : ''}${component.key}[${index}]`,
+                ));
               });
             }
-          });
-        }
+            else if (['form'].includes(component.type)) {
+              promises.push(eachValue(
+                component.components,
+                _.get(data, `${component.key}.data`, {}),
+                fn,
+                context,
+                `${path ? `${path}.` : ''}${component.key}.data`,
+              ));
+            }
+            else if (
+              ['container'].includes(component.type) ||
+              (
+                component.tree &&
+                !['panel', 'table', 'well', 'columns', 'fieldset', 'tabs', 'form'].includes(component.type)
+              )
+            ) {
+              promises.push(eachValue(
+                component.components,
+                _.get(data, component.key),
+                fn,
+                context,
+                `${path ? `${path}.` : ''}${component.key}`,
+              ));
+            }
+            else {
+              promises.push(eachValue(component.components, data, fn, context, path));
+            }
+          }
+          else if (component.hasOwnProperty('columns') && Array.isArray(component.columns)) {
+            // Handle column like layout components.
+            component.columns.forEach((column) => {
+              promises.push(eachValue(column.components, data, fn, context, path));
+            });
+          }
+          else if (component.hasOwnProperty('rows') && Array.isArray(component.rows)) {
+            // Handle table like layout components.
+            component.rows.forEach((row) => {
+              if (Array.isArray(row)) {
+                row.forEach((column) => {
+                  promises.push(eachValue(column.components, data, fn, context, path));
+                });
+              }
+            });
+          }
           // Call the callback for each component.
           promises.push(fn({...context, data, component, path}));
-      });
+        });
+      }
 
       return Promise.all(promises);
     }
