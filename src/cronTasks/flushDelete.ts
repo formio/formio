09f60c7ddf2efdf837bf0a9fa7298c2adf32d48d
Alexander Kiseliov <kiselev20060@gmail.com>
2020-03-09 18:40:36 +0300

    Added support of multiple databases to flush deleted cron.

--- src/cronTasks/flushDelete.ts
+++ src/cronTasks/flushDelete.ts
@@ -1,3 +1,4 @@
+import { Model } from '@formio/api';
 import { Formio } from '../Formio';
 
 const MONTH = 2678400000;
@@ -7,6 +8,8 @@ const MONTH = 2678400000;
  * @param app
  */
 export default async (app: Formio) => {
+  const contextModelName = process.env.CONTEXT_MODEL_NAME || '';
+
   const period = parseInt(process.env.FLUSH_DELETE_PERIOD, 10) || MONTH;
   const timestamp = Date.now() - period;
 
@@ -14,10 +17,69 @@ export default async (app: Formio) => {
     deleted: { $lt: timestamp },
   };
 
-  const deletePromises = Object.keys(app.models)
-    .map((modelName: string) => app.models[modelName].delete(query, {}, {
-      force: true,
-    }));
+  const models = getModels(app, contextModelName);
+  const contextItems = await findItems(models.context);
+  const globalDeletePromises = getDeletePromises(models.global, query, {});
 
-  return Promise.all(deletePromises);
+  let commonDeletePromises: Array<Promise<any>> = [];
+  if (!contextModelName || !contextItems.length) {
+    // If there is not context items - delete items with empty context
+    commonDeletePromises = getDeletePromises(models.common, query, {});
+  } else {
+    // Delete items with specified context
+    contextItems.forEach(({_id}) => {
+      const context = {};
+
+      if (_id) {
+        context[`${contextModelName}Id`] = _id.toString();
+      }
+
+      commonDeletePromises.push(
+        ...getDeletePromises(models.common, query, context),
+      );
+    });
+  }
+
+  return Promise.all([
+    ...globalDeletePromises,
+    ...commonDeletePromises,
+  ]);
 };
+
+const getModels = (app: Formio, contextModelName: string) => {
+  const models = {
+    context: null,
+    common: [],
+    global: [],
+  };
+
+  Object.keys(app.models).forEach((modelName: string) => {
+    const model: Model = app.models[modelName];
+
+    if (model.schema.name === contextModelName) {
+      models.context = model;
+    }
+
+    if (model.schema.global) {
+      models.global.push(model);
+    } else {
+      models.common.push(model);
+    }
+  });
+
+  return models;
+};
+
+const findItems = async (model: Model): Promise<any[]> => {
+  if (!model) {
+    return [];
+  }
+
+  return model.find({});
+};
+
+const getDeletePromises = (
+  models: Model[],
+  query = {},
+  context = {},
+): Array<Promise<any>> => models.map((model: Model) => model.delete(query, context, {force: true}));
