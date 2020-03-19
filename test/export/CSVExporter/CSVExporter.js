aa11cf5277a4abd04d5732a982d3e69bc3909814
Vlad <vlad.shusterman30@gmail.com>
2020-03-19 16:41:30 +0300

    Fix(CSVExport): tabs, code refactoring

--- test/export/CSVExporter/CSVExporter.js
+++ test/export/CSVExporter/CSVExporter.js
@@ -1,36 +1,35 @@
 module.exports = function(app, template, hook) {
-    var docker = process.env.DOCKER;
-    var Helper = require('../../helper')(app);
-    var helper = null;
-    const test = require('../../fixtures/forms/datetime-format.js');
+  const docker = process.env.DOCKER;
+  const Helper = require('../../helper')(app);
+  let helper = null;
+  const test = require('../../fixtures/forms/datetime-format.js');
 
   describe('CSVExporter', () => {
-      it('Sets up a default project', function(done) {
-        let owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
-        helper = new Helper(owner);
-          helper.project().user('user', 'user1').execute(done);
-      });
+    it('Sets up a default project', (done) => {
+      let owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
+      helper = new Helper(owner);
+        helper.project().user('user', 'user1').execute(done);
+    });
 
-      it (`Export works in case when format is not set`, (done) => {
-        let owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
-        helper = new Helper(owner);
-        helper.project().user('user', 'user1').execute(() => {
-          helper
-            .form('test', test.components)
-            .execute(function(err) {
-              if (err) {
-                return done(err);
-              }
-
-              helper.getExport(helper.template.forms.test,'csv', function(error, result) {
-                if (error) {
-                  done(error);
-                }
-                if (helper.template.forms.test.components[0].components[0].format) {
-                  done();
-                }
-              });
-            });
+    it (`Export works in case when format is not set`, (done) => {
+      let owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
+      helper = new Helper(owner);
+      helper
+        .project()
+        .form('test', test.components)
+        .submission({data: {dateTime: '2020-03-10T09:00:00.000Z'}})
+        .execute((err) => {
+          if (err) {
+            return done(err);
+          }
+          helper.getExport(helper.template.forms.test,'csv', (error, result) => {
+            if (error) {
+              done(error);
+            }
+            if (result.text.split('\n')[1].split(',')[3]) {
+              done();
+            }
+          });
         });
     })
   })
