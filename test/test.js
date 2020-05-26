e8c8bbdab6b62531860d6c9b1bf5822c91ebe767
Randall Knutson <randall@knutson.io>
2020-05-26 14:55:52 -0500

    Update formio.js to fix isomorphic validation.

--- test/test.js
+++ test/test.js
@@ -60,7 +60,6 @@ describe('Initialization', function() {
   after(function() {
     require('./templates')(app, template, hook);
     require('./bootstrap')(app, template, hook);
-    require('./unit')(app, template, hook);
     require('./auth')(app, template, hook);
     require('./roles')(app, template, hook);
     require('./form')(app, template, hook);
@@ -70,5 +69,6 @@ describe('Initialization', function() {
     require('./submission-access')(app, template, hook);
     require('./submission')(app, template, hook);
     require('./export/CSVExporter/CSVExporter')(app, template, hook);
+    require('./unit')(app, template, hook);
   });
 });
