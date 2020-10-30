a553ccb882cb2721bca2e0da626fcb115f944375
alexandraRamanenka <sasha@form.io>
2020-10-30 17:23:29 +0300

    Added hook to configure Formio instance

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ const util = require('./src/util/util');
 require('colors');
 require('./server')().then(function(state) {
   util.log(` > Serving the Form.io API Platform at ${state.config.domain.green}`);
+  const hook = require('./src/util/hook')(state.server.formio);
+  hook.alter('configFormio', util.Formio);
   state.server.listen(state.config.port);
 });
