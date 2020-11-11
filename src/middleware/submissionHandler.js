0d2de38bf3e0e58291dd1d10eb2ea02c21edf06c
MaksimFalei <Maksim.Falei@softensity.com>
2020-11-11 12:01:24 +0300

    Fixed typo

--- src/middleware/submissionHandler.js
+++ src/middleware/submissionHandler.js
@@ -99,7 +99,7 @@ module.exports = (router, resourceName, resourceId) => {
           req.body.data = {};
         }
 
-        // Esure that the _fvid is number.
+        // Ensure that the _fvid is number.
         if (req.body.hasOwnProperty('_fvid') && !_.isNaN(parseInt(req.body._fvid))) {
           req.body._fvid = parseInt(req.body._fvid);
         }
