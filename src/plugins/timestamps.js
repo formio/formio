98ab0c7fc9b020f179c0fe73c66167ecbca0868c
Travis Tidwell <travis@form.io>
2020-12-01 22:57:56 -0600

    Adding expiring action items.

--- src/plugins/timestamps.js
+++ src/plugins/timestamps.js
@@ -1,26 +1,28 @@
 'use strict';
 
-module.exports = function(schema, options) {
+module.exports = (schema, options = {}) => {
+  const created = {
+    type: Date,
+    index: true,
+    description: 'The date this resource was created.',
+    default: Date.now,
+    __readonly: true,
+  };
+
+  if (options.expires) {
+    created.expires = options.expires;
+  }
+
   // Add the created and modified params.
   schema.add({
-    created: {
-      type: Date,
-      description: 'The date this resource was created.',
-      'default': Date.now,
-      __readonly: true
-    },
+    created,
     modified: {
       type: Date,
+      index: true,
       description: 'The date this resource was modified.',
       __readonly: true
     }
   });
-
-  // If we wish to make these an index.
-  if (options && options.index) {
-    schema.path('created').index(options.index);
-    schema.path('modified').index(options.index);
-  }
 
   // On pre-save, we will update the modified date.
   schema.pre('save', function(next) {
