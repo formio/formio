4df7193b8ed4d295c74142b4805864152c1f3182
Alex <tkachez@gmail.com>
2020-12-08 11:39:35 +0300

    Added middleware that ensures that we are using proper collection for our request

--- src/middleware/submissionHandler.js
+++ src/middleware/submissionHandler.js
@@ -75,8 +75,10 @@ module.exports = (router, resourceName, resourceId) => {
 
       // If this is a get method, then filter the model query.
       if (isGet) {
-        req.countQuery = req.countQuery || req.model || this.model;
-        req.modelQuery = req.modelQuery || req.model || this.model;
+        const submissionModel = hook.alter('submissionCollection', req);
+
+        req.countQuery = submissionModel || req.countQuery || req.model || this.model;
+        req.modelQuery = submissionModel || req.modelQuery || req.model || this.model;
         if (req.handlerName !== 'beforeGet') {
           // Set the model query to filter based on the ID.
           req.countQuery = req.countQuery.find({form: req.currentForm._id});
