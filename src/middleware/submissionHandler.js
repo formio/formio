24e481bdc30b2d1dd555cde2d8c4c4ca30c54b38
Alex <tkachez@gmail.com>
2020-12-08 11:39:35 +0300

    updated build

--- src/middleware/submissionHandler.js
+++ src/middleware/submissionHandler.js
@@ -80,7 +80,12 @@ module.exports = (router, resourceName, resourceId) => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         const submissionModel = hook.invoke('submissionCollection', req);
+=======
+        const submissionModel = hook.alter('submissionCollection', req);
+
+>>>>>>> 4df7193... Added middleware that ensures that we are using proper collection for our request
 =======
         const submissionModel = hook.alter('submissionCollection', req);
 
