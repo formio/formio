93502f91cc5c7650c2f8dc38771358044448c92b
Alex <tkachez@gmail.com>
2021-01-06 11:19:26 +0300

    now using cache.loadCurrentForm over findOne

--- src/middleware/getMongoCollection.js
+++ src/middleware/getMongoCollection.js
@@ -8,30 +8,19 @@
  * @returns {Function}
  */
 module.exports = (router) => function(req,res,next) {
-    const hook = require('../util/hook')(router.formio);
-    const util = router.formio.util;
-
-    // Build the dynamic mongoose query.
-    const query = {
-        _id: util.idToBson(req.formId),
-        deleted: {$eq: null}
-    };
-
-    router.formio.resources.form.model.findOne(
-        hook.alter('formQuery', query, req)
-    ).lean().exec((err, result) => {
+    router.formio.cache.loadCurrentForm(req, (err, form) => {
         if (err) {
             return next();
         }
-        if (!result) {
+        if (!form) {
             return next();
         }
-        if (!(result.settings && result.settings.collection)) {
+        if (!(form.settings && form.settings.collection)) {
             return next();
         }
 
         const projectName = req.currentProject.name.replace(/[^A-Za-z0-9]+/g, '');
-        const collection = result.settings.collection;
+        const collection = form.settings.collection;
         const submissionModel = router.formio.mongoose.model(
             `${projectName}_${collection}`,
             router.formio.mongoose.modelSchemas.submission,
