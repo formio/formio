a4cdc459f8c8a21a34c72c7ee022fd2d189f3cb8
Maria-Golomb <mariagolomb.mg@gmail.com>
2021-01-15 13:38:27 +0300

    FJS-794: Added markModifiedParameters method to utils.

--- src/resources/FormResource.js
+++ src/resources/FormResource.js
@@ -79,15 +79,12 @@ module.exports = function(router) {
         router.formio.middleware.formLoader,
         router.formio.middleware.formActionHandler('after'),
         router.formio.middleware.filterResourcejsResponse(['deleted', '__v']),
-        router.formio.middleware.filterIndex(['components'])
+        router.formio.middleware.filterIndex(['components', 'properties'])
       ],
       hooks: {
         put: {
           before(req, res, item, next) {
-            if (item.components) {
-              item.markModified('components');
-            }
-
+            util.markModifiedParameters(item, ['components', 'properties']);
             return next();
           }
         }
