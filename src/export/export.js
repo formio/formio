f5a2560bb3ccfce7bea6d3430983b8b2085e3ae4
Aleh Katsiuba <olegkatuba@gmail.com>
2021-09-22 16:03:15 +0300

    fix export JSON/CSV doesn't work

--- src/export/export.js
+++ src/export/export.js
@@ -87,6 +87,10 @@ module.exports = (router) => {
         if (!components) {
           return Promise.resolve(components);
         }
+        if (components.noRecurse) {
+          return Promise.resolve(components);
+        }
+        components.noRecurse = true;
         const newComponents = components.map(component => {
           if (component.type === 'form' && component.form) {
             const subForm = router.formio.mongoose.models.form.findById(component.form)
