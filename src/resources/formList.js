2c6e8d6aaf153fd58e85c88ef20e7d4c5bca381e
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-05-20 23:06:11 +0530

    fixed regex of title

--- src/resources/formList.js
+++ src/resources/formList.js
@@ -1,18 +1,17 @@
 
 const formList =(req,res,router)=>{
     let query={delete:{$eq:null}};
+    let titleQuery={}
     const skipForm=req.query.skip||0;
     const limitForm=req.query.limit||0;
     const sortForm = req.query.sort&&req.query.sort==='-title'?-1:1
-    let regex = req.query?.title__regex&&req.query?.title__regex.split("/")||''
-    console.log(regex,"tttttttttt")
-    if(regex) regex= regex.filter(i=>i!=='')
-    let titleQuery =regex?{title:{$regex:`${regex[0]}`,$options:`${regex[1]}`}}:{}
     if(req.query){
         const {type,tags,title__regex}= req.query
         query={...query,type,tags:tags.split(","),}
+        let regex = title__regex?title__regex.split("/").filter(i=>i):''
+        titleQuery =regex?{title:{$regex:`${regex[0]}`,$options:`${regex[1]}`}}:{}
+
     }
-    console.log(sortForm)
     router.formio.resources.form.model.find({...query,...titleQuery}).skip(skipForm).limit(limitForm).sort({title:sortForm}).then(result=>{
         res.json(result)
     }).catch(err=>{
