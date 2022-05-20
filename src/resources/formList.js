1fa084881e99cab48f8eaf1e21a3f4e90d1f80b7
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-05-20 19:18:55 +0530

    updated with regx

--- src/resources/formList.js
+++ src/resources/formList.js
@@ -1,10 +1,22 @@
 
 const formList =(req,res,router)=>{
-    const decodedToken = req.token
-    const common = {delete:{$eq:null},type:'form'}
-    const query= decodedToken.tag? {"tags":decodedToken.tag,...common}:{...common}
-    router.formio.resources.form.model.find(query).then(result=>{
+    let query={delete:{$eq:null}};
+    const skipForm=req.query.skip||0;
+    const limitForm=req.query.limit||0;
+    const sortForm = req.query.sort&&req.query.sort==='-title'?-1:1
+    let regex = req.query?.title__regex&&req.query?.title__regex.split("/")||''
+    console.log(regex,"tttttttttt")
+    if(regex) regex= regex.filter(i=>i!=='')
+    let titleQuery =regex?{title:{$regex:`${regex[0]}`,$options:`${regex[1]}`}}:{}
+    if(req.query){
+        const {type,tags,title__regex}= req.query
+        query={...query,type,tags:tags.split(","),}
+    }
+    console.log(sortForm)
+    router.formio.resources.form.model.find({...query,...titleQuery}).skip(skipForm).limit(limitForm).sort({title:sortForm}).then(result=>{
         res.json(result)
+    }).catch(err=>{
+        res.status(403).json(err)
     })
 }
 
