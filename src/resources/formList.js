143313bd606c833bba885ba139e0b2f3df082658
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-05-30 22:27:59 +0530

    pagination fixed

--- src/resources/formList.js
+++ src/resources/formList.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const formList = (req, res, router) => {
+  const range= req.get('range')
   let query = { deleted: { $eq: null } };
   let titleQuery = {};
-  const skipForm = req.query.skip || 0;
-  const limitForm = req.query.limit || 0;
+  const skipForm = req.query.skip ||range&&range.split("-")[0] || 0;
+  const limitForm = req.query.limit || range&&Number(range.split("-")[1])+1||  0;
   const sortForm = req.query.sort && req.query.sort === "-title" ? -1 : 1;
   let { type, tags, title__regex } = req.query;
   type = type ? { type } : {};
@@ -21,30 +22,33 @@ const formList = (req, res, router) => {
     tenantQuery = {tenantKey };
   }
   
-  // const allForms = new Promise((resolve,reject)=>{
-  //   router.formio.resources.form.model
-  //   .find({ ...query, ...titleQuery,...tenantQuery})
-  //   .skip(skipForm)
-  //   .limit(limitForm)
-  //   .sort({ title: sortForm })
-  //   .lean().then((result)=>resolve(result)).catch(err=>reject(err))
-  // })
-  // const formCount = new Promise((resolve,reject)=>{
-  //   router.formio.resources.form.model
-  //   .find({ ...query, ...titleQuery,...tenantQuery})
-  //   .count().then((result)=>resolve(result)).catch(err=>reject(err))
-  // })
-
-  router.formio.resources.form.model
+  const allForms = new Promise((resolve,reject)=>{
+    router.formio.resources.form.model
     .find({ ...query, ...titleQuery,...tenantQuery})
     .skip(skipForm)
     .limit(limitForm)
     .sort({ title: sortForm })
-    .lean().then((result)=>{
-      res.json(result)
-    }).catch(err=>{
-      res.json(err)
-    })
+    .lean().then((result)=>resolve(result)).catch(err=>reject(err))
+  })
+  const formCount = new Promise((resolve,reject)=>{
+    router.formio.resources.form.model
+    .find({ ...query, ...titleQuery,...tenantQuery})
+    .count().then((result)=>resolve(result)).catch(err=>reject(err))
+  })
+
+  Promise.all([allForms,formCount]).then((result)=>{
+    const rangeOf = range?range:`${skipForm}-${(Number(limitForm)+Number(skipForm)-1)}`
+    const newRange=  `${rangeOf}/${result[1]}`
+    res.setHeader('Accept-Ranges','items')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Range, Accept-Ranges, Range-Unit')
+    res.setHeader('content-range',newRange)
+    res.json(result[0])
+  }).catch((err)=>{
+    console.log(err)
+    res.status(403).json(err)
+  })
+
+  
      
 };
 
