cb29bc6ce055730e322945a50525f54b18bde09f
Travis Tidwell <travis@form.io>
2022-06-08 14:17:12 -0500

    Revert "FIO-5090: updated tests"
    This reverts commit 76701cfbffbf8bbf1808cfc3aadd96f94b4fb0d0.

--- test/templates.js
+++ test/templates.js
@@ -235,7 +235,6 @@ module.exports = (app, template, hook) => {
 
     describe('Empty Template', function() {
       let testTemplate = require('../src/templates/empty.json');
-      testTemplate.revisions = {};
       let _template = _.cloneDeep(testTemplate);
 
       describe('Import', function() {
@@ -349,7 +348,6 @@ module.exports = (app, template, hook) => {
 
     describe('Default Template', function() {
       let testTemplate = require('../src/templates/default.json');
-      testTemplate.revisions = {};
       let _template = _.cloneDeep(testTemplate);
 
       describe('Import', function() {
