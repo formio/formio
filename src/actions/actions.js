3428138b6c968e81671e602f8fe606c65bfb79cc
lane-formio <lane@form.io>
2023-10-26 21:40:15 -0500

    FIO-7510: Revert isolated vm changes (#1650)
    * Revert "pull from main branch of isolated vm; fix linting errors"
    
    This reverts commit 99aa1f817192b23a0b50f6a6faaca50e94834765.
    
    * Revert "newIsolate function renamed"
    
    This reverts commit 0cf703427d63e970f64d109cb03797d8c166e51b.
    
    * Revert "using isolate factory"
    
    This reverts commit 1d88fa761c062a148aada5dcc0cbe43319360b2d.
    
    * Revert "vm2 removed"
    
    This reverts commit d7526fe9ebcc33c4a42a1155941382ccb8f4e62f.
    
    * Revert "Added vm-utils"
    
    This reverts commit ed7c96a5504e905964dd249b37f6696ee46caae4.
    
    * Revert "vm2 replaced with isolated-vm"
    
    This reverts commit 3ad0a1ca1ea6d9f90c7e1edf1ac2cc6e9fd91ea6.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,7 @@
 
 const Resource = require('resourcejs');
 const async = require('async');
-const vmUtil = require('vm-utils');
+const {VM} = require('vm2');
 const _ = require('lodash');
 const debug = {
   error: require('debug')('formio:error'),
@@ -219,7 +219,6 @@ module.exports = (router) => {
       });
     },
 
-    // eslint-disable-next-line max-statements
     async shouldExecute(action, req) {
       const condition = action.condition;
       if (!condition) {
@@ -251,25 +250,31 @@ module.exports = (router) => {
             _
           }, req);
 
-          const isolate = vmUtil.getIsolate();
-          const context = await isolate.createContext();
-          await vmUtil.transfer('execute', params.execute, context);
-          await vmUtil.transfer('query', params.query, context);
-          await vmUtil.transfer('data', params.data, context);
-          await vmUtil.transfer('form', params.form, context);
-          await vmUtil.transfer('submission', params.submission, context);
-          await vmUtil.transfer('previous', params.previous, context);
+          let vm = new VM({
+            timeout: 500,
+            sandbox: {
+              execute: params.execute,
+              query: params.query,
+              data: params.data,
+              form: params.form,
+              submission: params.submission,
+              previous: params.previous,
+            },
+            eval: false,
+            fixAsync: true
+          });
 
-          await vmUtil.freeze('jsonLogic', params.jsonLogic, context);
-          await vmUtil.freeze('util', params.FormioUtils, context);
-          await vmUtil.freeze('moment', params.moment, context);
-          await vmUtil.freeze('_', params._, context);
+          vm.freeze(params.jsonLogic, 'jsonLogic');
+          vm.freeze(params.FormioUtils, 'util');
+          vm.freeze(params.moment, 'moment');
+          vm.freeze(params._, '_');
 
-          const result = await context.eval(json ?
+          const result = vm.run(json ?
             `execute = jsonLogic.apply(${condition.custom}, { data, form, _, util })` :
-            condition.custom,
-            {timeout: 500, copy: true}
+            condition.custom
           );
+
+          vm = null;
 
           return result;
         }
