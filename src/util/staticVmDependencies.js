5690cc0e2c7d2d3d200ba230be7b7b705d3f0ef2
Brendan Bond <brendanbond@users.noreply.github.com>
2024-04-05 12:25:15 -0500

    add resolve dep (#1732)

--- src/util/staticVmDependencies.js
+++ src/util/staticVmDependencies.js
@@ -1,11 +1,12 @@
 'use strict';
 const fs = require('fs');
+const resolve = require('resolve/sync');
 
-const lodashCode = fs.readFileSync(require.resolve('lodash/lodash.min.js'), 'utf8');
-const momentCode = fs.readFileSync(require.resolve('moment/min/moment.min.js'), 'utf8');
-const formioCoreCode = fs.readFileSync(require.resolve('@formio/core/dist/formio.core.min.js'), 'utf8');
-const fastJsonPatchCode = fs.readFileSync(require.resolve('fast-json-patch/dist/fast-json-patch.min.js'), 'utf8');
-const nunjucksCode = fs.readFileSync(require.resolve('nunjucks/browser/nunjucks.min.js'), 'utf8');
+const lodashCode = fs.readFileSync(resolve('lodash/lodash.min.js'), 'utf8');
+const momentCode = fs.readFileSync(resolve('moment/min/moment.min.js'), 'utf8');
+const formioCoreCode = fs.readFileSync(resolve('@formio/core/dist/formio.core.min.js'), 'utf8');
+const fastJsonPatchCode = fs.readFileSync(resolve('fast-json-patch/dist/fast-json-patch.min.js'), 'utf8');
+const nunjucksCode = fs.readFileSync(resolve('nunjucks/browser/nunjucks.min.js'), 'utf8');
 
 module.exports = {
     lodash: lodashCode,
