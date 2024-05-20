4e0cb157d07dc70633db96ad39802468c8872063
Roman Letsuk <romaletsuk@gmail.com>
2024-05-20 13:49:21 +0300

    FIO-8343: fixed the issue with dereferencing data table components

--- src/resources/Validator.js
+++ src/resources/Validator.js
@@ -4,7 +4,8 @@ const {
   ProcessTargets,
   process,
   interpolateErrors,
-  escapeRegExCharacters
+  escapeRegExCharacters,
+  Utils,
 } = require('@formio/core');
 const {evaluateProcess} = require('@formio/vm');
 const util = require('../util/util');
@@ -194,7 +195,20 @@ class Validator {
     if (!resource) {
       throw new Error(`Resource at ${resourceId} not found for dereferencing`);
     }
-    return resource.components || [];
+    const dataTableComponents = (component.fetch.components || [])
+      .map(component => Utils.getComponent(resource.components || [], component.path));
+
+    const filterComponents = (component) => {
+      if (!component.components) {
+        return component;
+      }
+      component.components = component.components
+        .map(component => filterComponents(component))
+        .filter(component => dataTableComponents.includes(component) || component.components?.length > 0);
+      return component;
+    };
+
+    return filterComponents(resource).components || [];
   }
 
   /**
