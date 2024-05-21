bc247119e5539c2b3064ebe39a9f55b02f4a7f89
mikekotikov <kmikecoder@gmail.com>
2024-05-21 13:04:01 +0200

    FIO-6966: Fix address submission in CSV when manual mode is enabled, update tests

--- src/export/exporters/CSVExporter.js
+++ src/export/exporters/CSVExporter.js
@@ -60,6 +60,13 @@ class CSVExporter extends Exporter {
 
         // If a component has multiple parts, pick what we want.
         if (component.type === 'address') {
+          const getAddressComponentValue = (component, value = {}) => {
+            if (component.enableManualMode && value.address) {
+              return value.address;
+            }
+            return value;
+          };
+
           items.push({
             rename: (label) => `${label}.formatted`,
             preprocessor: (value) => {
@@ -67,7 +74,7 @@ class CSVExporter extends Exporter {
                 return value;
               }
 
-              const address =  value || {};
+              const address = getAddressComponentValue(component, value);
 
               // OpenStreetMap || Azure || Google
               // eslint-disable-next-line max-len
@@ -81,7 +88,7 @@ class CSVExporter extends Exporter {
                 return value;
               }
 
-              const address = value || {};
+              const address = getAddressComponentValue(component, value);
 
               // OpenStreetMap || Azure || Google
               return address.lat || _.get(address, 'position.lat') || _.get(address, 'geometry.location.lat') || '';
@@ -94,7 +101,7 @@ class CSVExporter extends Exporter {
                 return value;
               }
 
-              const address = value || {};
+              const address = getAddressComponentValue(component, value);
 
               // OpenStreetMap || Azure || Google
               return address.lon || _.get(address, 'position.lon') || _.get(address, 'geometry.location.lng') || '';
@@ -423,7 +430,6 @@ class CSVExporter extends Exporter {
       res.status(400).send(err.message || err);
     }
   }
-  /* eslint-enable max-statements */
 
   /**
    * Start the CSV export by creating the headers.
@@ -496,7 +502,6 @@ class CSVExporter extends Exporter {
     const updatedSubmission = {};
     const result = this.fields.map((column) => {
       const componentData = _.get(submission.data, column.path);
-
       // If the path had no results and the component specifies a path, check for a datagrid component
       if (_.isUndefined(componentData) && column.path.includes('.')) {
         let parts = column.path.split('.');
--- test/fixtures/forms/azureAddressComponent.js
+++ test/fixtures/forms/azureAddressComponent.js
@@ -1,98 +1,262 @@
 module.exports = {
-  components: [
+
+  "components": [
     {
-      label: "Address",
-      enableManualMode: true,
-      tableView: true,
-      defaultValue: {},
-      allowCalculateOverride: true,
-      provider: "azure",
-      validate: {
-        json: "\"\""
+      "label": "Address",
+      "enableManualMode": true,
+      "tableView": true,
+      "defaultValue": {
+        "mode": "autocomplete",
+        "address": {}
       },
-      errors: "\"\"",
-      key: "address",
-      conditional: {
+      "allowCalculateOverride": true,
+      "provider": "azure",
+      "validate": {
         "json": "\"\""
       },
-      type: "address",
-      providerOptions: {
-        params: {
-          "subscription-key": "AZURE_API_KEY"
+      "errors": "\"\"",
+      "key": "addressWithMode",
+      "conditional": {
+        "json": "\"\""
+      },
+      "type": "address",
+      "providerOptions": {
+        "params": {
+          "subscription-key": "GmlF67m8ZDnjjIltC4yg9snIis5jaRhjRfq-nmvszgc"
         }
       },
-      input: true,
+      "input": true,
+      "components": [
+        {
+          "label": "Address 1",
+          "tableView": false,
+          "key": "address1",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Address 2",
+          "tableView": false,
+          "key": "address2",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "City",
+          "tableView": false,
+          "key": "city",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "State",
+          "tableView": false,
+          "key": "state",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Country",
+          "tableView": false,
+          "key": "country",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Zip Code",
+          "tableView": false,
+          "key": "zip",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        }
+      ]
     },
     {
-      type: "button",
-      label: "Submit",
-      key: "submit",
-      disableOnInvalid: true,
-      input: true,
-      tableView: false,
+      "label": "Address2",
+      "tableView": false,
+      "provider": "azure",
+      "key": "address",
+      "type": "address",
+      "providerOptions": {
+        "params": {
+          "autocompleteOptions": {},
+          "subscription-key": "GmlF67m8ZDnjjIltC4yg9snIis5jaRhjRfq-nmvszgc"
+        }
+      },
+      "input": true,
+      "components": [
+        {
+          "label": "Address 1",
+          "tableView": false,
+          "key": "address1",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Address 2",
+          "tableView": false,
+          "key": "address2",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "City",
+          "tableView": false,
+          "key": "city",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "State",
+          "tableView": false,
+          "key": "state",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Country",
+          "tableView": false,
+          "key": "country",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        },
+        {
+          "label": "Zip Code",
+          "tableView": false,
+          "key": "zip",
+          "type": "textfield",
+          "input": true,
+          "customConditional": "show = _.get(instance, 'parent.manualMode', false);"
+        }
+      ]
     },
+    {
+      "key": "submit",
+      "type": "button",
+      "input": true,
+      "label": "Submit",
+      "tableView": false,
+      "disableOnInvalid": true
+    }
   ],
+
   submission: {
     "data": {
-      "textField": "Test Address",
-      "address": {
+      "addressWithMode": {
         "mode": "autocomplete",
-        "address":{
+        "address": {
           "type": "Geography",
-          "id": "TbOmYMFBz7Hm8q_lxc-oCA",
-          "score": 2.3817019463,
-          "entityType": "Municipality",
+          "id": "Pu0OhJmmMboGreDYM87aIw",
+          "score": 1,
+          "entityType": "CountrySubdivision",
           "matchConfidence": {
             "score": 1
           },
           "address": {
-            "municipality": "Tokyo",
-            "countrySubdivision": "Kanto",
-            "countryCode": "JP",
-            "country": "Japan",
-            "countryCodeISO3": "JPN",
-            "freeformAddress": "Tokyo, Kanto"
+            "countrySubdivision": "WA",
+            "countrySubdivisionName": "Washington",
+            "countrySubdivisionCode": "WA",
+            "countryCode": "US",
+            "country": "United States",
+            "countryCodeISO3": "USA",
+            "freeformAddress": "Washington"
           },
           "position": {
-            "lat": 35.68696,
-            "lon": 139.74946
+            "lat": 47.37319,
+            "lon": -120.4237
           },
           "viewport": {
             "topLeftPoint": {
-              "lat": 37.14388,
-              "lon": 138.37411
+              "lat": 49.00249,
+              "lon": -124.76258
             },
             "btmRightPoint": {
-              "lat": 34.89988,
-              "lon": 140.87509
+              "lat": 45.54354,
+              "lon": -116.91599
             }
           },
           "boundingBox": {
             "topLeftPoint": {
-              "lat": 37.14388,
-              "lon": 138.37411
+              "lat": 49.00249,
+              "lon": -124.76258
             },
             "btmRightPoint": {
-              "lat": 34.89988,
-              "lon": 140.87509
+              "lat": 45.54354,
+              "lon": -116.91599
             }
           },
           "dataSources": {
             "geometry": {
-              "id": "00005858-5800-1200-0000-00007d30cf7f"
+              "id": "00005858-5800-1200-0000-000077361cb4"
             }
+          }
+        }
+      },
+      "address": {
+        "type": "Geography",
+        "id": "al9oTvbe7VmKoP3ql8XHKA",
+        "score": 1,
+        "entityType": "Municipality",
+        "matchConfidence": {
+          "score": 1
+        },
+        "address": {
+          "municipality": "New York",
+          "countrySubdivision": "NY",
+          "countrySubdivisionName": "New York",
+          "countrySubdivisionCode": "NY",
+          "countryCode": "US",
+          "country": "United States",
+          "countryCodeISO3": "USA",
+          "freeformAddress": "New York, NY"
+        },
+        "position": {
+          "lat": 40.71305,
+          "lon": -74.00723
+        },
+        "viewport": {
+          "topLeftPoint": {
+            "lat": 40.9175,
+            "lon": -74.25909
           },
-          "address1": "",
-          "address2": "",
-          "city": "",
-          "state": "",
-          "country": "",
-          "zip": ""
+          "btmRightPoint": {
+            "lat": 40.47738,
+            "lon": -73.70027
+          }
+        },
+        "boundingBox": {
+          "topLeftPoint": {
+            "lat": 40.9175,
+            "lon": -74.25909
+          },
+          "btmRightPoint": {
+            "lat": 40.47738,
+            "lon": -73.70027
+          }
+        },
+        "dataSources": {
+          "geometry": {
+            "id": "d49d86a4-0f4b-45f2-b607-31a84d02af00"
+          }
         }
       },
       "submit": true
-    },
-    "state": "submitted",
-    "_vnote": ""
-  }
-};
+
+
+    }
+  },
+  "state": "submitted",
+  "_vnote": ""
+}
+
