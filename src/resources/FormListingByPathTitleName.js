600b2fa51697e50f476ed9d7db719bf26826b648
shuhaib s <95394061+shuhaib-aot@users.noreply.github.com>
2024-08-29 16:33:10 +0530

    Feature/adde select in search endpoint (#94)
    * Added new endpoint 'forms/search' for listing forms by path or name or title
    
    * Select query parameter added for selecting field

--- src/resources/FormListingByPathTitleName.js
+++ src/resources/FormListingByPathTitleName.js
@@ -6,7 +6,7 @@ module.exports = {
         if(!req.isAdmin){
           res.status(401).json({message:"unauthorized"})
         }
-        const { title, path, name } = req.query;
+        const { title, path, name, select } = req.query;
         const condition = [];
 
         if (title) condition.push({ title });
@@ -16,7 +16,15 @@ module.exports = {
         if (condition.length === 0) {
           return res.status(400).json({ message: "No query parameters provided." });
         }
-        const forms = await router.formio.resources.form.model.find({ $or: condition , deleted:{$eq: null}});
+ 
+        let projection = {};
+        if (select) {
+          projection = select.split(",").reduce((acc, field) => {
+            acc[field] = 1; 
+            return acc;
+          }, {});
+        } 
+        const forms = await router.formio.resources.form.model.find({ $or: condition , deleted:{$eq: null}},projection);
         return res.json(forms);
       } catch (error) {
         next(error);
