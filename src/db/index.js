5a8e849e2a6feff1490daca30e214750ff726bd0
brendanjbond <brendanbond@gmail.com>
2024-12-12 12:35:07 -0600

    fix tls options

--- src/db/index.js
+++ src/db/index.js
@@ -205,7 +205,7 @@ module.exports = function(formio) {
     }
     if (config.mongoSA || config.mongoCA) {
       mongoConfig.tls = true;
-      mongoConfig.sslCA = config.mongoSA || config.mongoCA;
+      mongoConfig.tlsCA = config.mongoSA || config.mongoCA;
     }
     if (config.mongoSSL) {
       mongoConfig = {
