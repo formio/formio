0cab6bf012c65290e8606982bef66041288a260d
Brendan J Bond <brendanbond@gmail.com>
2025-05-04 11:19:12 -0500

    update bundles

--- src/vm/entries/core-lodash-moment-inputmask-nunjucks.js
+++ src/vm/entries/core-lodash-moment-inputmask-nunjucks.js
@@ -3,8 +3,8 @@
 const FormioCore = require("@formio/core");
 const _ = require("lodash");
 const moment = require("moment");
-const inputmask = require("inputmask");
 const nunjucks = require('nunjucks');
+const inputmask = require('inputmask');
 const {RootShim} = require('../src/RootShim');
 const {InstanceShim} = require('../src/InstanceShim');
 const {Event} = require('../src/polyfill');
@@ -17,11 +17,11 @@ module.exports = {
   InstanceShim,
   Event,
   moment,
-  inputmask,
   _,
   nunjucks,
   environment,
   sanitize,
   unescape,
-  dateFilter
+  dateFilter,
+  inputmask
 };
--- src/vm/entries/core-lodash-moment-inputmask.js
+++ src/vm/entries/core-lodash-moment-inputmask.js
@@ -3,9 +3,9 @@
 const FormioCore = require("@formio/core");
 const _ = require("lodash");
 const moment = require("moment");
-const inputmask = require("inputmask");
+const inputmask = require('inputmask');
 const {RootShim} = require('../src/RootShim');
 const {InstanceShim} = require('../src/InstanceShim');
 const {Event} = require('../src/polyfill');
 
-module.exports = {util: FormioCore.Utils, utils: FormioCore.Utils, RootShim, InstanceShim, Event, moment, inputmask, _};
+module.exports = {util: FormioCore.Utils, utils: FormioCore.Utils, RootShim, InstanceShim, Event, moment, _, inputmask};
