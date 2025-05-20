73d896c37ab8896a35d96f2e010e1d933dd52cdd
Brendan Bond <brendanbond@users.noreply.github.com>
2025-05-20 10:19:13 -0700

    FIO-9783: refactor evaluator for performance (4.5.x branch) (#1980)
    * WIP: refactor vm to quickjs
    
    * update .eslintignore to include vm
    
    * move to isolated-vm
    
    * WIP: a working example of IsolateVM as in the Evaluator class. Although working, takes some shortcuts with @formio/js, so do not use this commit until fixed
    
    * update vm dependency
    
    * solidify the vm build process
    
    * add email rendering; refactor vm
    
    * lint datefilter
    
    * update core dep
    
    * minor changes
    
    * update vm dependency
    
    * update dependencies; update isolatevm evaluator to support form modules
    
    * change vm directory structure; update core dependency
    
    * update core dependency
    
    * update core dependency
    
    * rebuild vm
    
    * update vm dep
    
    * update module logic in isolatevmevaluator
    
    * webhook action and evaluator should interpolate without evaluations
    
    * update core dependency
    
    * add more tests
    
    * update bundles
    
    * update instanceshim
    
    * whoopsie
    
    * don't pass full context to evaluateSync
    
    * update formio.js dependency
    
    * update lockfile; do not delete configFormio hook
    
    * update core, vm, js dependencies
    
    ---------
    
    Co-authored-by: lane-formio <lane@form.io>

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const util = require('./src/util/util');
+
 require('colors');
 require('./server')().then(function(state) {
   util.log(` > Serving the Form.io API Platform at ${state.config.domain.green}`);
