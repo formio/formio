6d89a828cf4b123274050735cd842268721a395d
Travis Tidwell <travis@form.io>
2025-07-01 13:31:26 -0500

    Fixed tests.

--- src/resources/Validator.js
+++ src/resources/Validator.js
@@ -354,6 +354,7 @@ class Validator {
       context.rules = FormioCore.rules.concat(context.rules);
       context.scope = await FormioCore.process(context);
       submission.data = context.data;
+      submission.scope = context.scope;
 
       // Now that the validation is complete, we need to remove fetched data from the submission.
       for (const path in context.scope.fetched) {
